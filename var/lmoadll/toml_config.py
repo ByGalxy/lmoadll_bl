fc3d55abaeb1a3d031359780ad943d58e35b1124
ByGalxy <1242352258@qq.com>
2025-09-08 21:39:23 +0800

    del: 移除外部cdn

--- var/lmoadll/toml_config.py
+++ var/lmoadll/toml_config.py
@@ -3,16 +3,15 @@ import os
 import tomllib
 
 
-
-config_path = 'config.toml'
+config_path = "config.toml"
 
 
 # 检查配置文件是否存在并读取
-def Doesitexist_configtoml(a,b):
+def Doesitexist_configtoml(a, b):
     if not os.path.exists(config_path):
         return False
     else:
-        with open(config_path,'rb') as f:
+        with open(config_path, "rb") as f:
             config = tomllib.load(f)
 
         if not config[a][b]:
